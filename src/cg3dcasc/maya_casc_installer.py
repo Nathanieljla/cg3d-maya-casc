f7e95128c20f3d6ea82ed4732c7f96761c539ef1
Nathanieljla <nathanieljla@gmail.com>
2023-11-01 08:06:14 -0500

    small fix to installer try except

--- src/cg3dcasc/maya_casc_installer.py
+++ src/cg3dcasc/maya_casc_installer.py
@@ -1223,14 +1223,13 @@ class MyInstaller(ModuleManager):
             from cg3dguru.utils import menu_maker
             menu_maker.run(menu_namespace='cg3dcasc.menu')
         except Exception as e:
-            import traceback
-            from pathlib import Path        
-            import maya.cmds as cmds
-            module_path = cmds.moduleInfo(path=True, moduleName='cascadeur')
+            import traceback       
+            #import maya.cmds as cmds
+            #module_path = cmds.moduleInfo(path=True, moduleName='cascadeur')
             print("\n\n")
             print("--------------------------------------------------------")
             print(e)
-            log = Path(module_path).parent.joinpath('cascadeur', 'scripts', 'error.log')
+            log = pathlib.Path(self.scripts_path).joinpath('error.log')
             print(log)
             callstack = traceback.format_exc()
             print(callstack)
