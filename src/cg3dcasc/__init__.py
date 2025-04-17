d7a49876f91b042536708fbc3cc881e65b89a47d
Artstation2024\Anderson <nathanieljla@gmail.com>
2025-04-17 18:58:06 -0500

    Incremented to version 1.5

--- src/cg3dcasc/__init__.py
+++ src/cg3dcasc/__init__.py
@@ -2,7 +2,7 @@ from .core import *
 from . import preferences
 #from .core import hik
 
-VERSION = (1, 1, 3)
+VERSION = (1, 5, 0)
 
 __author__ = "Nathaniel Albright"
 __email__ = "developer@3dcg.guru"
