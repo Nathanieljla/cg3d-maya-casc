3671b1ee241a97f689a5a472ead9fe4896e27317
Nathanieljla <nathanieljla@gmail.com>
2026-07-27 10:49:45 -0500

    updated menu command names to support new format

--- src/cg3dcasc/casc-site/cg3dcmds/create_new_set.py
+++ src/cg3dcasc/casc-site/cg3dcmds/create_new_set.py
@@ -2,6 +2,9 @@ def command_name():
     return "Maya.Export Sets.Create New Set"
 
 
+def name():
+    return command_name()
+
 def run(scene):
     import cg3dmaya
     cg3dmaya.add_selection_to(True)
--- src/cg3dcasc/casc-site/cg3dcmds/sync_ids.py
+++ src/cg3dcasc/casc-site/cg3dcmds/sync_ids.py
@@ -1,6 +1,8 @@
 def command_name():
     return "Maya.Export Sets.Sync Maya ID"
 
+def name():
+    return command_name()
 
 def run(scene):
     import cg3dmaya
